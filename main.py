af0700977f1957d29637a6283ef466ca02195ae6
Mazy <landonp95@gmail.com>
2022-05-30 16:01:34 -0400

    pp

--- main.py
+++ main.py
@@ -3,13 +3,9 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 load_dotenv()
-import globals
-
-if __name__ == "__main__":
-  globals.initialize()
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix=">>", intents=intents, debug_guilds=globals.ids)
+bot = commands.Bot(command_prefix=">>", intents=intents, debug_guilds=[os.getenv("TESTID")])
 
 for filename in os.listdir("cogs"):
   if filename.endswith(".py"):
