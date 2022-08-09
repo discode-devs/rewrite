6bfd8877be6250f2a3c4c8710fa1f3371aaf12c6
Mazylol <landonp95@gmail.com>
2022-08-09 16:24:32 -0400

    I dislike comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,10 +40,10 @@ tasks {
                 "processResources",
                 "compileKotlin"
             )
-        ) // We need this for Gradle optimization to work
-        archiveClassifier.set("standalone") // Naming the jar
+        )
+        archiveClassifier.set("standalone")
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
         val sourcesMain = sourceSets.main.get()
         val contents = configurations.runtimeClasspath.get()
             .map { if (it.isDirectory) it else zipTree(it) } +
