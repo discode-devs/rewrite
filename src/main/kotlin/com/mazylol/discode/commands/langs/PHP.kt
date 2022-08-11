3fc70e5421a9f854d91f1e11a3b5b53b1ce148ac
mazylol <landonp95@gmail.com>
2022-08-11 15:27:15 -0400

    optimize imports

--- src/main/kotlin/com/mazylol/discode/commands/langs/PHP.kt
+++ src/main/kotlin/com/mazylol/discode/commands/langs/PHP.kt
@@ -2,7 +2,7 @@ package com.mazylol.discode.commands.langs
 
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent
-import java.util.*
+import java.util.Date
 
 object PHP {
     fun phpembed(event : SelectMenuInteractionEvent) {
