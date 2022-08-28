91e0e0e1963921bb6dd9411d297fd486db525cee
Mazy <landonp95@gmail.com>
2022-08-28 15:27:07 -0400

    svelte command

--- src/main/kotlin/com/mazylol/discode/commands/frameworks/FrameworkDropDown.kt
+++ src/main/kotlin/com/mazylol/discode/commands/frameworks/FrameworkDropDown.kt
@@ -2,6 +2,7 @@ package com.mazylol.discode.commands.frameworks
 
 import com.mazylol.discode.commands.frameworks.Angular.angularembed
 import com.mazylol.discode.commands.frameworks.React.reactembed
+import com.mazylol.discode.commands.frameworks.Svelte.svelteembed
 import com.mazylol.discode.commands.frameworks.Vue.vueembed
 import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
@@ -18,6 +19,8 @@ class FrameworkDropDown : ListenerAdapter() {
                     .withEmoji(Emoji.fromFormatted("<:framework_angular:1007363681350336612>"))
                 ).addOptions(SelectOption.of("React", "react")
                     .withEmoji(Emoji.fromFormatted("<:framework_react:1007363752997421056>"))
+                ).addOptions(SelectOption.of("Svelte", "svelte")
+                    .withEmoji(Emoji.fromFormatted("<:framework_svelte:1013527172289409134>"))
                 ).addOptions(SelectOption.of("Vue", "vue")
                     .withEmoji(Emoji.fromFormatted("<:framework_vue:1007363822530605157>"))
                 )
@@ -34,6 +37,7 @@ class FrameworkDropDown : ListenerAdapter() {
         when(event.values[0]) {
             "angular" -> angularembed(event)
             "react" -> reactembed(event)
+            "svelte" -> svelteembed(event)
             "vue" -> vueembed(event)
             else -> event.reply("Invalid (somehow)")
         }
---
+++ src/main/kotlin/com/mazylol/discode/commands/frameworks/Svelte.kt
@@ -0,0 +1,22 @@
+package com.mazylol.discode.commands.frameworks
+
+import net.dv8tion.jda.api.EmbedBuilder
+import net.dv8tion.jda.api.events.interaction.component.SelectMenuInteractionEvent
+import java.util.Date
+
+object Svelte {
+    fun svelteembed(event: SelectMenuInteractionEvent) {
+        val eb = EmbedBuilder()
+        eb.setTitle("Svelte")
+        eb.setDescription("The framework of champions!")
+        eb.setThumbnail("https://raw.githubusercontent.com/discode-devs/rewrite/master/src/main/resources/images/frameworks/svelte.png")
+        eb.addField("Overview", "Svelte is a free and open-source front end compiler created by Rich Harris and maintained by the Svelte core team members. Svelte compiles HTML templates to specialized code that manipulates the DOM directly, which can often reduce the size of files and give better performance. It also has an accompanying framework called SvelteKit.", false)
+        eb.addField("Features", "- Speed \n- Ease of use\n- Flexibility\n- Simple to learn", false)
+        eb.addField("Learn", "https://svelte.dev/tutorial/basics\nhttps://www.freecodecamp.org/news/the-svelte-handbook/", false)
+        eb.addField("Sources", "https://en.wikipedia.org/wiki/Svelte\nhttps://svelte.dev/", false)
+        eb.setTimestamp(Date().toInstant())
+        eb.setColor(5793266)
+        eb.setFooter("Discode", "https://raw.githubusercontent.com/discode-devs/rewrite/master/src/main/resources/images/botatar.png")
+        event.replyEmbeds(eb.build()).queue()
+    }
+}
