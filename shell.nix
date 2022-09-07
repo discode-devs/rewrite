f7088455239f6ce059601bdbd70c25d6523a2582
Mazy <landonp95@gmail.com>
2022-09-06 22:04:05 -0400

    shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
-{ pkgs ? import <nixpkgs> {  } };
+{ pkgs ? import <nixpkgs> {  } }:
 with pkgs;
 mkShell {
-  name = "discode-env";
   buildInputs = [ git jdk jre gradle ];
   shellHook = ''
-    export PS1='\n\[\033[1;34m\][${name}:\w]\$\[\033[0m\] '
+    export PS1='\n\[\033[1;34m\][discode-env:\w]\$\[\033[0m\] '
   '';
 }
