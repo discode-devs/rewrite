298677a648a07203793015bfdf6329376e1c0e49
Mazy <landonp95@gmail.com>
2022-10-28 12:24:01 +0000

    information inserted, image moved to correct place, need to add emoji id

--- src/main/kotlin/com/mazylol/discode/commands/people/KenThompson.kt
+++ src/main/kotlin/com/mazylol/discode/commands/people/KenThompson.kt
@@ -9,7 +9,13 @@ object KenThompson {
         val eb = EmbedBuilder()
         eb.setTitle("Ken Thompson")
         eb.setDescription("B, Unix, and Go God")
-        eb.addField("Oveview", "", false)
+        eb.setThumbnail("https://raw.githubusercontent.com/discode-devs/rewrite/master/src/main/resources/images/people/kenthompson.png")
+        eb.addField("Oveview", "Kenneth Lane Thompson is an American computer scientist. He designed and implemented the original Unix operating system. He also create the B language which was the predecessor to C. Since 2006 he has worked at Google where he has co-developed the Go programming language.", false)
+        eb.addField("Life and Death", "Born: Feburary 4, 1943\nDied: -", false)
+        eb.addField("Sources", "https://en.wikipedia.org/wiki/Ken_Thompson", false)
+        eb.setTimestamp(Date().toInstant())
+        eb.setColor(5793266)
+        eb.setFooter("Discode", "https://raw.githubusercontent.com/discode-devs/rewrite/master/src/main/resources/images/botatar.png")
         event.replyEmbeds(eb.build()).queue()
     }
 }
